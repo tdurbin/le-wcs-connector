0181608a576c98b25014a36d3bd642d9a2369733
tdurbin <tdurbin@liveperson.com>
2018-01-04 22:12:37 +0000

    initial commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 require('dotenv').config();
 
 // This section is for the deployment of the connector on heroku
+// You can comment this out when running locally.
 // *************************************************************
 var http = require('http');
 http.createServer(function (req, res) {
