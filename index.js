93d1a458f50ae01c5a30e121186ed28a03ea0fd4
tdurbin <tdurbin@liveperson.com>
2018-04-24 14:13:46 +0100

    initial commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ http.createServer(function(req, res) {
 
 // ping heroku every 10 minutes to keep the connector alive
 setInterval(function() {
-    http.get("http://telco-wcs-connector.herokuapp.com");
+    http.get("http://mels-connector.herokuapp.com");
 }, 600000);
 // *************************************************************
 
