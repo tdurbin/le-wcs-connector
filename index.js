5cea976238f84ab13a070a358a44df966067e149
tdurbin <tdurbin@liveperson.com>
2018-05-01 09:22:08 +0100

    initial commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ http.createServer(function(req, res) {
 
 // ping heroku every 10 minutes to keep the connector alive
 setInterval(function() {
-    http.get("http://rbm-wcs-connector.herokuapp.com");
+    http.get("http://td-wcs-connector.herokuapp.com");
 }, 600000);
 // *************************************************************
 
