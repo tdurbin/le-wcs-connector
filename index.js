7b187135a5100f82ae8e760b9c6ebb571c1eddf8
tdurbin <tdurbin@liveperson.com>
2018-10-02 22:49:31 +0100

    commit

--- index.js
+++ index.js
@@ -187,11 +187,10 @@ function processResponse(err, response, dialogID) {
 
                         if (response.output.endpoint.delay_multiplier !== "undefined") {
                             var delayTotal = response.output.endpoint.delay_multiplier * snippetdelay;
+                            console.log('Delay time     : ' + delayTotal);
                         } else {
                             var delayTotal = 0;
                         }
-
-                        console.log('Delay time     : ' + delayTotal);
 
                         setTimeout(function() {
 
